62b73396cafa70c8c49af4b23141d0fa5741230e
Nils Fischer <n.fischer@viwid.com>
2015-05-08 16:57:28 +0200

    setup empty application project

---
+++ viewhierarchy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  viewhierarchy
+//
+//  Created by Nils Fischer on 08.05.15.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        NSLog("Hello World!")
+        
+        return true
+    }
+
+
+}
+
